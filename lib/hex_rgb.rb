e88f8b352552eb41a814d1ba7518078a41ab319c
Andrew M Westling <a@andrewwestling.com>
2014-08-03 22:09:35 -0700

    add interpret logic

--- lib/hex_rgb.rb
+++ lib/hex_rgb.rb
@@ -1,125 +1,82 @@
 # thinking ahead: regex for validation of input... 
 # /([\d|([a-fA-F])]{6})/
 
-
-@letters =  {   
-                10 => "a",
-                11 => "b",
-                12 => "c",
-                13 => "d",
-                14 => "e",
-                15 => "f",
-            }
-
-
 def hex_rgb input
 
-  colors = break_into_colors(input) # colors is now a hash with hex values
-  base10 = convert_to_base10(colors) # makes a hash of base10 values from hex values
-  result = build_result(base10) # makes a result string from the hash of base10 values
+  hex_hash = break_apart(input) 
+  rgb_hash = convert_to_rgb(hex_hash) 
+  rgb = interpret(rgb_hash) 
 
-  result
+  rgb
 
 end
 
 
-def break_into_colors input
+def break_apart input
   # breaks up the #f1553a input into {"r" => ["f",1], "g" => [5,5], "b" => [3,"a"]}
 
-  colors = { :r => [], :g => [], :b => []}
+  hex_hash = { :r => [], :g => [], :b => []}
   
   input.downcase!
   input_array = input.split('')
 
-  colors[:r] << input_array[0]
-  colors[:r] << input_array[1]
-  colors[:g] << input_array[2]
-  colors[:g] << input_array[3]
-  colors[:b] << input_array[4]
-  colors[:b] << input_array[5]
+  hex_hash[:r] << input_array[0]
+  hex_hash[:r] << input_array[1]
+  hex_hash[:g] << input_array[2]
+  hex_hash[:g] << input_array[3]
+  hex_hash[:b] << input_array[4]
+  hex_hash[:b] << input_array[5]
 
-  colors
+  hex_hash
 
 end
 
 
-def convert_to_base10 colors
-  # takes the breaks_into_colors result and converts each value into a base10 number
-  # input looks like {:r => ["f","1"], :g => ["5","5"], :b => ["3","a"]}
-  # output looks like {:r => 240, :g => 103 :b => 66}
+def convert_to_rgb hex_hash
 
-  output = {:r => 0, :g => 0, :b => 0}
+  @letters =  {   
+                  10 => "a",
+                  11 => "b",
+                  12 => "c",
+                  13 => "d",
+                  14 => "e",
+                  15 => "f",
+              }
+  # takes the break_apart result and converts each value into a base10 number
+  # input (hex_hash) looks like {:r => ["f","1"], :g => ["5","5"], :b => ["3","a"]}
+  # output (rgb_hash) looks like {:r => 241, :g => 85 :b => 58}
 
-  # input = {:r => ["f","1"], :g => ["5","5"], :b => ["3","a"]}
+  rgb_hash = {:r => 0, :g => 0, :b => 0}
 
+  hex_hash.each_pair do |key, value|
 
+    value.reverse!
 
-  # get it to return 241 for ["f","1"]
-
-  # red = ["f","1"]
-  # red.reverse! ["f", "1"] --> ["1", "f"]
-
-  # red.each_with_index do |value, i|
-  # test each item in red
-
-  # if @letters.has_value?(value)
-    # @letters.key(value)*16**index
-  # index 0
-  # output[:r] += 1 * 16**index
-  # index 1
-  # output[:r] += 15 * 16**index
-
-
-
-
-  [:r, :g, :b].each do |key|
-    colors[key].reverse!
-  end
-
-  colors.each_key do |color|
-
-    (0..1).each do |i|
-
-      if color[i] == "1"
-        output[color] += 1*16**i
-      elsif color[i] == "2"
-        output[color] += 2*16**i
-      elsif color[i] == "3"
-        output[color] += 3*16**i
-      elsif color[i] == "4"
-        output[color] += 4*16**i
-      elsif color[i] == "5"
-        output[color] += 5*16**i
-      elsif color[i] == "6"
-        output[color] += 6*16**i
-      elsif color[i] == "7"
-        output[color] += 7*16**i
-      elsif color[i] == "8"
-        output[color] += 8*16**i
-      elsif color[i] == "9"
-        output[color] += 9*16**i
-      elsif color[i] == "a"
-        output[color] += 10*16**i
-      elsif color[i] == "b"
-        output[color] += 11*16**i
-      elsif color[i] == "c"
-        output[color] += 12*16**i
-      elsif color[i] == "d"
-        output[color] += 13*16**i
-      elsif color[i] == "e"
-        output[color] += 14*16**i
-      elsif color[i] == "f"
-        output[color] += 15*16**i
+    value.each_with_index do |value, i|  
+      if @letters.has_value?(value)
+        rgb_hash[key] += @letters.key(value) * 16**i
+      else
+        value_f = value.to_f
+        rgb_hash[key] += value_f * 1 * 16**i
       end
-    end    
+    end
   end
-  puts output
 
-  
-  # [colors[:r], colors[:g], colors[:b]].each do |color|
-  #   color.split('')
-  # end
+  rgb_hash
 
 end
 
-convert_to_base10({:r => ["f","1"], :g => ["5","5"], :b => ["3","a"]})
+
+def interpret rgb_hash
+  # rgb_hash looks like {:r => 241, :g => 85 :b => 58}
+
+
+  rgb_string = ""
+
+  rgb_hash.each_pair do |color, value|
+    rgb_string << "#{color.upcase}: #{value.to_i}, "
+  end
+
+  rgb_string.chop!.chop!
+
+end
--- spec/hex_rgb_spec.rb
+++ spec/hex_rgb_spec.rb
@@ -2,16 +2,23 @@ require 'rspec'
 require 'hex_rgb'
 
 describe 'hex_rgb' do
-  describe 'break_into_colors' do
+  describe 'break_apart' do
     it "takes an input and builds a hash with arrays as values for r, g, and b" do
-      expect(break_into_colors("f1553a")).to include(:r => ["f","1"])
-      expect(break_into_colors("f1553a")).to include(:g => ["5","5"])
-      expect(break_into_colors("f1553a")).to include(:b => ["3","a"])
+      expect(break_apart("f1553a")).to include(:r => ["f","1"])
+      expect(break_apart("f1553a")).to include(:g => ["5","5"])
+      expect(break_apart("f1553a")).to include(:b => ["3","a"])
     end
   end
-  describe 'convert_to_base10' do
-    it "takes output of break_into_colors and builds a hash of r, g, b values" do
-      expect hex_rgb("f1553a").to eq "255,0,0"
+  describe 'convert_to_rgb' do
+    it "takes output of break_apart and builds a hash of r, g, b values" do
+      expect(convert_to_rgb({:r => ["f","1"], :g => ["5","5"], :b => ["3","a"]})).to include(:r => 241)
+      expect(convert_to_rgb({:r => ["f","1"], :g => ["5","5"], :b => ["3","a"]})).to include(:g => 85)
+      expect(convert_to_rgb({:r => ["f","1"], :g => ["5","5"], :b => ["3","a"]})).to include(:b => 58)
+    end
+  end
+  describe 'interpret' do
+    it "takes the rgb_hash as an input and turns it into a string that displays in the UI" do
+      expect(interpret({:r=>241.0, :g=>85.0, :b=>58.0})).to eq "R: 241, G: 85, B: 58"
     end
   end
 end
